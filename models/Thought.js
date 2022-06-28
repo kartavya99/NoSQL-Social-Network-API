521016a46e74eefde9e182001a5c6d81cd3b91af
kartavya <kartavyas@gmail.com>
2022-06-28 12:18:29 +1000

    updated models and added connection.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,23 +1,34 @@
 const { Schema } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: [1, "Not enough character"],
-    maxlength: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: [1, "Not enough character"],
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Dae,
+      default: Date.now,
+      // get:
+    },
+    username: {
+      type: String,
+      required: [true, "Please enter your username"],
+    },
+    // * `reactions` (These are like replies)
+    //   * Array of nested documents created with the `reactionSchema`
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Dae,
-    default: Date.now,
-    // get:
-  },
-  username: {
-    type: String,
-    required: [true, "Please enter your username"],
-  },
-  // * `reactions` (These are like replies)
-  //   * Array of nested documents created with the `reactionSchema`
-});
+  {
+    //getters: true according to mini project
+    toJSON: {},
+    id: false,
+  }
+);
+
+// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
 
 module.export = thoughtSchema;
--- models/User.js
+++ models/User.js
@@ -1,35 +1,52 @@
-
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,            
-        },
-         email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: 
-            // [\w._%+-]+@[\w.-]+\.[a-zA-z]{2,4}, 
-            // "Please enter a valid email address"],
-        },
-        // * `thoughts`
-        // * Array of `_id` values referencing the `Thought` model
-
-        //  friends`
-        //   * Array of `_id` values referencing the `User` model (self-reference)
-
-        
-
-    }
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true, "Please enter a username!"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+      // match:
+      // [\w._%+-]+@[\w.-]+\.[a-zA-z]{2,4},
+      // "Please enter a valid email address"],
+    },
+    // * `thoughts`
+    // * Array of `_id` values referencing the `Thought` model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    //  friends`
+    //   * Array of `_id` values referencing the `User` model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+//Initialize User model
+const User = model("user", userSchema);
+
 module.export = userSchema;
-
-
-
-
