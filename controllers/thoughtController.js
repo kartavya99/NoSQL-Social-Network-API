021ebb74c53a7da061e13435c34c0b36f3d24b10
kartavya <kartavyas@gmail.com>
2022-06-28 22:57:57 +1000

    getSingleThought and deletethoght route added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,6 @@
 const { ObjectId } = require("mongoose");
 const { User, Thought } = require("../models");
 
-// **`/api/thoughts`**
-// * `GET` to get all thoughts
-
 // @desc get thoughts data
 // @route GET /api/thoughts
 
@@ -16,17 +13,26 @@ const getAllThoughts = async (req, res) => {
   }
 };
 
-// * `GET` to get a single thought by its `_id`
+// @desc Get thought by id
+// @route GET /api/thoughts/thoughtId
+const getSingleThought = async (req, res) => {
+  try {
+    const thought = await Thought.findOne({ _id: req.params.thoughtId }).select(
+      "-__V"
+    );
+
+    if (!thought) {
+      res.status(400).json({ message: "No thought with that ID" });
+    }
+    res.status(200).json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 // * `POST` to create a new thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
 
 // ```json
-// // example data
-// {
-//   "thoughtText": "Here's a cool thought...",
-//   "username": "lernantino",
-//   "userId": "5edff358a0fcb779aa7b118b"
-// }
 
 // @desc create thought data
 // @route POST /api/thoughts
@@ -38,19 +44,44 @@ const createThought = async (req, res) => {
       userId: req.body.userId,
     });
     res.json(thought);
+    // // example data to create thought
+    // {
+    //   "thoughtText": "Here's a cool thought...",
+    //   "username": "lernantino",
+    //   "userId": "5edff358a0fcb779aa7b118b"
+    // }
   } catch (err) {
     res.status(500).json(err);
   }
 };
 
 // * `PUT` to update a thought by its `_id`
+
 // * `DELETE` to remove a thought by its `_id`
 
+const deleteThought = async (req, res) => {
+  try {
+    const thought = await Thought.findOneAndDelete({
+      _id: req.params.thoughtId,
+    });
+
+    if (!thought) {
+      res.status(404).json({ message: "No thought with that ID" });
+    }
+    res.status(200).json({ message: "Thought deleted" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
 // **`/api/thoughts/:thoughtId/reactions`**
+
 // * `POST` to create a reaction stored in a single thought's `reactions` array field
 // * `DELETE` to pull and remove a reaction by the reaction's `reactionId` value
 
 module.exports = {
   getAllThoughts,
   createThought,
+  getSingleThought,
+  deleteThought,
 };
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -3,9 +3,13 @@ const router = require("express").Router();
 const {
   getAllThoughts,
   createThought,
+  getSingleThought,
+  deleteThought,
 } = require("../../controllers/thoughtController");
 
 router.route("/").get(getAllThoughts);
 router.route("/").post(createThought);
+router.route("/:thoughtId").get(getSingleThought);
+router.route("/:thoughtId").delete(deleteThought);
 
 module.exports = router;
